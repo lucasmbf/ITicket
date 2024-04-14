4e0765cfae630a4402d45e4e24659e4468cbdfb3
lucasmbf <lucasmbfonseca@gmail.com>
2024-04-14 20:37:12 -0300

    Versao para apresentação da Sprint2

--- ITicket/Controllers/HomeController.cs
+++ ITicket/Controllers/HomeController.cs
@@ -3,11 +3,38 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using SQLitePCL;
+using System.Linq;
+using System.Security.Claims;
 
 namespace ITicket.Controllers {
-    public class HomeController : Controller {
-      
+    public class HomeController : Controller
+    {
+        private readonly ContextoDb _contexto;
 
+        public HomeController(ContextoDb contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public IActionResult MinhaConta()
+{
+        // resgata o nome do usuario da sessao atual
+        var username = HttpContext.Session.GetString("Username");
+
+        // procura o usuario no db
+        var usuario = _contexto.Usuario.FirstOrDefault(u => u.Username == username);
+
+        // caso nao seja encontrado, redireciona para pag de login
+        if (usuario == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        // passa o usuario para a view
+        return View(usuario);
+    }
+        
         public IActionResult Index() {
               var username = HttpContext.Session.GetString("Username");          
              if(username == null){
@@ -20,6 +47,7 @@ namespace ITicket.Controllers {
             return View();
         }
 
+        
         public IActionResult Login() {
 
             return View();
